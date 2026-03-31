97c0371f87407fdb34138065a6f25875b54d7c3d
michikatsu000 <antonvsivcev2@gmail.com>
2026-03-31 15:30:07 +0300

    Task20

--- Program.cs
+++ Program.cs
@@ -1,323 +1,196 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
-using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
 
-public class MyTreeMap<K, V>
+namespace Graph
 {
-    private class Node
+    internal class Program
     {
-        public K key;
-        public V value;
-        public Node Left;
-        public Node Right;
-        public Node(K key, V value)
-        {
-            this.key = key;
-            this.value = value;
+        static int[,] TransitiveClosureBFS(List<int>[] graph) {
+            int n=graph.Length;
+            int[,] rechability = new int[n, n];
+            for (int source = 0; source < n; source++) {
+                bool[] visited = new bool[n];
+             Queue<int> queue=new Queue<int>();
+              queue.Enqueue(source);
+                visited[source] = true;
+                while (queue.Count > 0) {
+                  int current=queue.Dequeue();
+                    //Помечаем достижимость из source в current
+                    rechability[source, current] = 1;
+                    foreach (int neighbor in graph[current])
+                    {
+                        if (!visited[neighbor])
+                        {
+                            visited[neighbor] = true;
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+               }
+            }
+        return rechability;
         }
-    }
-    private IComparer<K> comparator;
-    private Node root;
-    private int size;
-    //1
-    public MyTreeMap()
-    {
-        comparator = Comparer<K>.Default;
-        root = null;
-        size = 0;
-    }
-    //2
-    public MyTreeMap(IComparer<K> comp)
-    {
-        comparator = comp ?? Comparer<K>.Default;
-        root = null;
-        size = 0;
-    }
+        class WeightEdge {
+        public int Vertex { get; set; }
+        public int Weight { get; set; }
+            public WeightEdge(int vertex, int weight) {
+                Vertex = vertex;
+                Weight = weight;
+            }
 
-    private int Compare(K k1, K k2)
-    {
-
-        return comparator.Compare(k1, k2);
-    }
-    //3
-    public void Clear()
-    {
-        root = null;
-        size = 0;
-    }
-
-    private Node GetNode(Node node, K key)
-    {
-        if (node == null) return null;
-        int cmp = Compare(key, node.key);
-        if (cmp == 0) return node;
-        if (cmp < 0) return GetNode(node.Left, key);
-        return GetNode(node.Right, key);
-    }
-    //4
-    public bool ContainsKey(K key)
-    {
-        return GetNode(root, key) != null;
-    }
-    //5 
-    public bool ContainsValue(V value)
-    {
-
-        return ContainsValue(root, value);
-    }
-    private bool ContainsValue(Node node, V value)
-    {
-        if (node == null) return false;
-        if (EqualityComparer<V>.Default.Equals(node.value, value))
-            return true;
-        return ContainsValue(node.Left, value) ||
-                ContainsValue(node.Right, value);
-
-    }
-
-
-
-    private void InOrder(Node node, List<KeyValuePair<K, V>> list)
-    {
-        if (node == null) return;
-        InOrder(node.Left, list);
-        list.Add(new KeyValuePair<K, V>(node.key, node.value));
-        InOrder(node.Right, list);
-
-
-    }
-    //6 
-    public List<KeyValuePair<K, V>> EntrySet()
-    {
-        var list = new List<KeyValuePair<K, V>>();
-        InOrder(root, list);
-        return list;
-
-    }
-    //7 
-    public V Get(K key)
-    {
-        var node = GetNode(root, key);
-        return node == null ? default(V) : node.value;
-    }
-    //8
-
-    public bool IsEmpty()
-    {
-        return size == 0;
-
-    }
-    //9
-    public List<K> KeySet()
-    {
-        var list = new List<K>();
-        foreach (var entry in EntrySet())
-            list.Add(entry.Key);
-        return list;
-    }
-    private Node Put(Node node, K key, V value)
-    {
-        if (node == null)
-        { size++; return new Node(key, value); }
-        int cmp = Compare(key, node.key);
-        if (cmp == 0) node.value = value;
-        else if (cmp < 0) node.Left = Put(node.Left, key, value);
-        else node.Right = Put(node.Right, key, value);
-        return node;
-    }
-    //10 
-    public void Put(K key, V value)
-    {
-
-        root = Put(root, key, value);
-    }
-    //11
-    public void Remove(K key)
-    {
-
-        root = Remove(root, key);
-    }
-    private Node Max(Node node)
-    {
-        while (node.Right != null) node = node.Right;
-        return node;
-
-
-    }
-    private Node Min(Node node)
-    { while (node.Left != null) node = node.Left; return node; }
-    private Node Remove(Node node, K key)
-    {
-        if (node == null) return null;
-        int cmp = Compare(key, node.key);
-        if (cmp < 0) node.Left = Remove(node.Left, key);
-        else if (cmp > 0) node.Right = Remove(node.Right, key);
-        else
-        {
-            size--;
-            if (node.Left == null) return node.Right;
-            if (node.Right == null) return node.Left;
-            Node min = Min(node.Right);
-            node.key = min.key;
-            node.value = min.value;
-            node.Right = Remove(node.Right, min.key);
         }
-        return node;
-    }
-    //12
-    public int Size() { return size; }
-    //13
-    public K FirstKey()
-    {
-        if (IsEmpty()) throw new InvalidOperationException("Map is empty");
-        return Min(root).key;
-    }
-    //14
-    public K LastKey()
-    {
-        if (IsEmpty()) throw new InvalidOperationException("Map is empty");
-        return Max(root).key;
-    }
-    //15
-    public MyTreeMap<K, V> HeadMap(K end)
-    {
-        var result = new MyTreeMap<K, V>(comparator);
-        foreach (var e in EntrySet())
-            if (Compare(e.Key, end) < 0) result.Put(e.Key, e.Value);
-        return result;
-    }
-    //16
-    public MyTreeMap<K, V> TailMap(K start)
-    {
-        var result = new MyTreeMap<K, V>(comparator);
-        foreach (var e in EntrySet())
-            if (Compare(e.Key, start) > 0)
-                result.Put(e.Key, e.Value);
-        return result;
-    }
-    //17
-    public MyTreeMap<K, V> SubMap(K start, K end)
-    {
-        var result = new MyTreeMap<K, V>(comparator);
-        foreach (var e in EntrySet())
-            if (Compare(e.Key, start) >= 0 &&
-                Compare(e.Key, end) < 0)
-                result.Put(e.Key, e.Value);
-        return result;
-    }
-    //18
-    public KeyValuePair<K, V>? LowerEntry(K key)
-    {
-        KeyValuePair<K, V>? result = null;
+        static int[] Dijkstra(List<WeightEdge>[] weightedGraph, int startVertex) {
+        int n=weightedGraph.Length;
+            int[] distances = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                distances[i]=int.MaxValue;
+            }
+            distances[startVertex] = 0;
+            bool[] visited=new bool [n];
 
-        foreach (var e in EntrySet())
-        {
-            if (Compare(e.Key, key) < 0)
-                result = e;
-            else
-                break;
+            var priorityQ = new SortedSet<(int distances, int vertex)>();
+            priorityQ.Add((0, startVertex));
+            while (priorityQ.Count > 0) {
+                var current = priorityQ.Min;
+                priorityQ.Remove(current);
+                int currentVerteex=current.vertex;
+                if (visited[currentVerteex]) continue;
+                visited[currentVerteex]=true;
+                foreach (var edge in weightedGraph[currentVerteex]) {
+                    int neighbor = edge.Vertex;
+                    int weight = edge.Weight;
+                    if (!visited[neighbor] && distances[currentVerteex] != int.MaxValue)
+                    {
+                        int newDistance = distances[currentVerteex] + weight;
+                        if (newDistance < distances[neighbor]) {
+                            priorityQ.Remove((distances[neighbor], neighbor));
+                            distances[neighbor]=newDistance;
+                            priorityQ.Add((distances[neighbor], neighbor));
+                        }    
+                    }
+                }
+            }
+            return distances;
+        }
+        class Edge {
+        public int To { get; set; }
+            public int ReverseIndex { get; set; }
+        public long Capacity { get; set; }
+            public Edge(int to, int reverseIndex, long capacity) {
+                To = to;
+                ReverseIndex=reverseIndex;
+                Capacity = capacity;
+            }
+        }
+        class DinicalAlgorithm {
+            private int verticesCount;
+            private List<Edge>[] graph;
+            private int[] level;
+            private int[] pointer;
+            public DinicalAlgorithm(int n)
+            {
+                verticesCount = n;
+                graph = new List<Edge>[verticesCount];
+                level = new int[verticesCount];
+                pointer = new int[verticesCount];
+                for (int i = 0; i < verticesCount; i++) {
+                    graph[i] = new List<Edge>();   
+                }
+            }
+            public void AddEdge(int from, int to, long capacity)
+            {
+
+                Edge forwardEdge = new Edge(to, graph[to].Count, capacity);
+                Edge backwardEdge = new Edge(from, graph[from].Count, 0);
+                graph[from].Add(forwardEdge);
+                graph[to].Add(backwardEdge);
+            }
+            private bool BFS(int source, int sink) {
+                for (int i = 0; i < verticesCount; i++) {
+                    level[i] = -1;
+                }
+            Queue<int> queue=new Queue<int>();
+            queue.Enqueue(source);
+                level[source] = 0;
+                while (queue.Count > 0) {
+                int current=queue.Dequeue();
+                    foreach (Edge edge in graph[current]) {
+                        if (edge.Capacity > 0 && level[edge.To] < 0)
+                        {
+                            level[edge.To] = level[current] + 1;
+                            queue.Enqueue(edge.To);
+                        }
+                    }
+                }
+                return level[sink] >= 0;
+
+             }
+            private long DFS(int current, int sink, long flow) {
+                if (current == sink) //дошли до стока дно рекурсии
+                    return flow;
+                for (; pointer[current] < graph[current].Count; pointer[current]++) {
+                Edge edge= graph[current][pointer[current]];
+                    if (edge.Capacity > 0 && level[current] + 1 == level[edge.To]) {
+                        long pushed = DFS(edge.To, sink, Math.Min(flow, edge.Capacity));
+                        if (pushed > 0) {
+                        edge.Capacity-= pushed;
+                            graph[edge.To][edge.ReverseIndex].Capacity += pushed;
+                            return pushed;
+                        }
+                    }
+                
+                }
+                return 0;
+            }
+            public long MaxFlow(int source, int sink) {
+                long maxFlow = 0;
+                while (true) {
+
+                    if (!BFS(source, sink))
+                        break;
+                    for (int i = 0; i < verticesCount; i++) {
+                        pointer[i] = 0;
+                    }
+                    while (true) {
+                        long pushed = DFS(source, sink, long.MaxValue);
+                        if (pushed == 0) break;
+                        maxFlow += pushed;
+                    
+                    
+                    
+                    
+                    }
+                
+                
+                }
+            return maxFlow;
+            
+            }
         }
 
-        return result;
-    }
-    //19 
-    public KeyValuePair<K, V>? FloorEntry(K key)
-    {
-        KeyValuePair<K, V>? result = null;
 
-        foreach (var e in EntrySet())
+        static void Main(string[] args)
         {
-            if (Compare(e.Key, key) <= 0)
-                result = e;
-            else
-                break;
+            // 1. Пример транзитивного замыкания
+    List<int>[] graph = new List<int>[4];
+            // ... заполнение графа
+            int[,] closure = TransitiveClosureBFS(graph);
+
+            // 2. Пример алгоритма Дейкстры
+            List<WeightEdge>[] weightedGraph = new List<WeightEdge>[5];
+            // ... заполнение взвешенного графа
+            int[] distances = Dijkstra(weightedGraph, 0);
+
+            // 3. Пример алгоритма Диница
+            DinicalAlgorithm dinic = new DinicalAlgorithm(6);
+            dinic.AddEdge(0, 1, 16);
+            // ... добавление ребер
+            long maxFlow = dinic.MaxFlow(0, 5);
+
         }
-
-        return result;
-    }
-    //20
-    public KeyValuePair<K, V>? HigherEntry(K key)
-    {
-        foreach (var e in EntrySet())
-        {
-            if (Compare(e.Key, key) > 0)
-                return e;
-        }
-
-        return null;
-    }
-    //21
-    public KeyValuePair<K, V>? CeilingEntry(K key)
-    {
-        foreach (var e in EntrySet())
-        {
-            if (Compare(e.Key, key) >= 0)
-                return e;
-        }
-
-        return null;
-    }
-    //22
-    public K LowerKey(K key)
-    {
-        var e = LowerEntry(key);
-        return e.HasValue ? e.Value.Key : default(K);
-    }
-    //23
-    public K FloorKey(K key)
-    {
-        var e = FloorEntry(key);
-        return e.HasValue ? e.Value.Key : default(K);
-    }
-    //24
-    public K HigherKey(K key)
-    {
-        var e = HigherEntry(key);
-        return e.HasValue ? e.Value.Key : default(K);
-    }
-    //25
-    public K CeilingKey(K key)
-    {
-        var e = CeilingEntry(key);
-        return e.HasValue ? e.Value.Key : default(K);
-    }
-    //26
-    public KeyValuePair<K, V>? PollFirstEntry()
-    {
-        if (IsEmpty()) return null;
-
-        var minNode = Min(root);
-        var result = new KeyValuePair<K, V>(minNode.key, minNode.value);
-
-        Remove(minNode.key);
-        return result;
-    }
-    //27
-    public KeyValuePair<K, V>? PollLastEntry()
-    {
-        if (IsEmpty()) return null;
-
-        var maxNode = Max(root);
-        var result = new KeyValuePair<K, V>(maxNode.key, maxNode.value);
-
-        Remove(maxNode.key);
-        return result;
-    }
-    //28
-    public KeyValuePair<K, V>? FirstEntry()
-    {
-        if (IsEmpty()) return null;
-
-        var minNode = Min(root);
-        return new KeyValuePair<K, V>(minNode.key, minNode.value);
-    }
-    //29 
-    public KeyValuePair<K, V>? LastEntry()
-    {
-        if (IsEmpty()) return null;
-
-        var maxNode = Max(root);
-        return new KeyValuePair<K, V>(maxNode.key, maxNode.value);
     }
 }
