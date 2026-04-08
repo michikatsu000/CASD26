094aacd7b9af01f50c14222b36deaa5104a1ad5d
michikatsu000 <antonvsivcev2@gmail.com>
2026-04-08 03:59:49 +0300

    Task_21

--- Program.cs
+++ Program.cs
@@ -1,196 +1,338 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Graph
+using System.Security.Policy;
+using System.Threading;
+//Про loadFactor
+//1 Если он маленький то много resize => много выделенной памяти но быстрый поиск мало коллизий
+//2 loadFactor =0.75 стандартный (золотая середина) приемлемое использование памяти и коливо колизий
+//3 loadFactor=0.95 экономия памяти но много коллизий
+public class MyHashMap<K, V>
 {
-    internal class Program
+    //Внутренний класс для хранения ключ-значения
+    private class Entry
     {
-        static int[,] TransitiveClosureBFS(List<int>[] graph) {
-            int n=graph.Length;
-            int[,] rechability = new int[n, n];
-            for (int source = 0; source < n; source++) {
-                bool[] visited = new bool[n];
-             Queue<int> queue=new Queue<int>();
-              queue.Enqueue(source);
-                visited[source] = true;
-                while (queue.Count > 0) {
-                  int current=queue.Dequeue();
-                    //Помечаем достижимость из source в current
-                    rechability[source, current] = 1;
-                    foreach (int neighbor in graph[current])
-                    {
-                        if (!visited[neighbor])
-                        {
-                            visited[neighbor] = true;
-                            queue.Enqueue(neighbor);
-                        }
-                    }
-               }
-            }
-        return rechability;
-        }
-        class WeightEdge {
-        public int Vertex { get; set; }
-        public int Weight { get; set; }
-            public WeightEdge(int vertex, int weight) {
-                Vertex = vertex;
-                Weight = weight;
-            }
-
-        }
-        static int[] Dijkstra(List<WeightEdge>[] weightedGraph, int startVertex) {
-        int n=weightedGraph.Length;
-            int[] distances = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                distances[i]=int.MaxValue;
-            }
-            distances[startVertex] = 0;
-            bool[] visited=new bool [n];
-
-            var priorityQ = new SortedSet<(int distances, int vertex)>();
-            priorityQ.Add((0, startVertex));
-            while (priorityQ.Count > 0) {
-                var current = priorityQ.Min;
-                priorityQ.Remove(current);
-                int currentVerteex=current.vertex;
-                if (visited[currentVerteex]) continue;
-                visited[currentVerteex]=true;
-                foreach (var edge in weightedGraph[currentVerteex]) {
-                    int neighbor = edge.Vertex;
-                    int weight = edge.Weight;
-                    if (!visited[neighbor] && distances[currentVerteex] != int.MaxValue)
-                    {
-                        int newDistance = distances[currentVerteex] + weight;
-                        if (newDistance < distances[neighbor]) {
-                            priorityQ.Remove((distances[neighbor], neighbor));
-                            distances[neighbor]=newDistance;
-                            priorityQ.Add((distances[neighbor], neighbor));
-                        }    
-                    }
-                }
-            }
-            return distances;
-        }
-        class Edge {
-        public int To { get; set; }
-            public int ReverseIndex { get; set; }
-        public long Capacity { get; set; }
-            public Edge(int to, int reverseIndex, long capacity) {
-                To = to;
-                ReverseIndex=reverseIndex;
-                Capacity = capacity;
-            }
-        }
-        class DinicalAlgorithm {
-            private int verticesCount;
-            private List<Edge>[] graph;
-            private int[] level;
-            private int[] pointer;
-            public DinicalAlgorithm(int n)
-            {
-                verticesCount = n;
-                graph = new List<Edge>[verticesCount];
-                level = new int[verticesCount];
-                pointer = new int[verticesCount];
-                for (int i = 0; i < verticesCount; i++) {
-                    graph[i] = new List<Edge>();   
-                }
-            }
-            public void AddEdge(int from, int to, long capacity)
-            {
-
-                Edge forwardEdge = new Edge(to, graph[to].Count, capacity);
-                Edge backwardEdge = new Edge(from, graph[from].Count, 0);
-                graph[from].Add(forwardEdge);
-                graph[to].Add(backwardEdge);
-            }
-            private bool BFS(int source, int sink) {
-                for (int i = 0; i < verticesCount; i++) {
-                    level[i] = -1;
-                }
-            Queue<int> queue=new Queue<int>();
-            queue.Enqueue(source);
-                level[source] = 0;
-                while (queue.Count > 0) {
-                int current=queue.Dequeue();
-                    foreach (Edge edge in graph[current]) {
-                        if (edge.Capacity > 0 && level[edge.To] < 0)
-                        {
-                            level[edge.To] = level[current] + 1;
-                            queue.Enqueue(edge.To);
-                        }
-                    }
-                }
-                return level[sink] >= 0;
-
-             }
-            private long DFS(int current, int sink, long flow) {
-                if (current == sink) //дошли до стока дно рекурсии
-                    return flow;
-                for (; pointer[current] < graph[current].Count; pointer[current]++) {
-                Edge edge= graph[current][pointer[current]];
-                    if (edge.Capacity > 0 && level[current] + 1 == level[edge.To]) {
-                        long pushed = DFS(edge.To, sink, Math.Min(flow, edge.Capacity));
-                        if (pushed > 0) {
-                        edge.Capacity-= pushed;
-                            graph[edge.To][edge.ReverseIndex].Capacity += pushed;
-                            return pushed;
-                        }
-                    }
-                
-                }
-                return 0;
-            }
-            public long MaxFlow(int source, int sink) {
-                long maxFlow = 0;
-                while (true) {
-
-                    if (!BFS(source, sink))
-                        break;
-                    for (int i = 0; i < verticesCount; i++) {
-                        pointer[i] = 0;
-                    }
-                    while (true) {
-                        long pushed = DFS(source, sink, long.MaxValue);
-                        if (pushed == 0) break;
-                        maxFlow += pushed;
-                    
-                    
-                    
-                    
-                    }
-                
-                
-                }
-            return maxFlow;
-            
-            }
-        }
-
-
-        static void Main(string[] args)
+        public K Key { get; set; }
+        public V Value { get; set; }
+        public Entry Next { get; set; }
+        public Entry(K key, V value)
         {
-            // 1. Пример транзитивного замыкания
-    List<int>[] graph = new List<int>[4];
-            // ... заполнение графа
-            int[,] closure = TransitiveClosureBFS(graph);
-
-            // 2. Пример алгоритма Дейкстры
-            List<WeightEdge>[] weightedGraph = new List<WeightEdge>[5];
-            // ... заполнение взвешенного графа
-            int[] distances = Dijkstra(weightedGraph, 0);
-
-            // 3. Пример алгоритма Диница
-            DinicalAlgorithm dinic = new DinicalAlgorithm(6);
-            dinic.AddEdge(0, 1, 16);
-            // ... добавление ребер
-            long maxFlow = dinic.MaxFlow(0, 5);
-
+            Key = key;
+            Value = value;
+            Next = null;
         }
     }
+    private Entry[] table;
+    private int size;
+    private float loadFactor;
+    private const int Default_Capacity = 16;
+    private const float Default_Load_Factor = 0.75f;
+
+    //1 Конструктор для создания пустого отображения 
+    // с начальной ёмкостью 16 элементов и коэффициентом загрузки 0.75
+    public MyHashMap() : this(Default_Capacity, Default_Load_Factor)
+    {
+
+    }
+    //2
+    public MyHashMap(int initialCapacity) : this(initialCapacity, Default_Load_Factor)
+    {
+
+
+    }
+    //3
+    public MyHashMap(int initialCapacity, float loadFactor)
+    {
+        if (initialCapacity <= 0) throw new ArgumentException("Ёмкость массива не может быть нулевой или отрицательной");
+        if (loadFactor <= 0 || float.IsNaN(loadFactor))
+            throw new ArgumentException("Коэффициент загрузки должен быть положительным");
+
+        this.table = new Entry[initialCapacity];
+        this.size = 0;
+        this.loadFactor = loadFactor;
+    }
+    //Вспомогательный метод для вычисления хешк-кода по объекту ключа
+    private int GetBucketIndex(K key)
+    {
+        if (key == null) return 0;
+        int hashCode = key.GetHashCode();
+        return Math.Abs(hashCode % table.Length);
+    }
+    //Метод для увелечения размера таблицы при превышении процента загржуенности (loadFactor)
+    private void ResizeIfNeeded()
+    {
+        if ((float)size / table.Length <= loadFactor)
+            return; //Если кол во реальных элементов к выделенной  памяти массива не превышает процент загрузки ничего не делаем
+        int newCapacity = table.Length * 2;
+        Entry[] newTable = new Entry[newCapacity];
+
+        for (int i = 0; i < table.Length; i++)
+        {
+            Entry current = table[i];
+            while (current != null)
+            {
+                Entry next = current.Next;
+                //Вычисляем новый индекс
+                int newIndex = current.Key == null ? 0 : Math.Abs(current.Key.GetHashCode() % newCapacity);
+                current.Next = newTable[newIndex]; //при коллизии указывает на то что уже записано next или на null если ничего не записано
+                newTable[newIndex] = current;  //становмтся новой головой списка в бакете
+                current = next;
+            }
+        }
+        table = newTable;
+
+    }
+    //resize может разрешать коллизии перераспределением элементов из старых бакетов в одиночные
+    //математически следует из формулы хеш кода
+    //где вариантов хеша становится больше относительно нового класса вычета
+    //4
+    public void Clear()
+    {
+        for (int i = 0; i < table.Length; i++)
+        {
+            table[i] = null;
+        }
+        size = 0;
+    }
+    //5
+    public bool ContainsKey(object key)
+    {
+        if (key == null)
+        {
+            int index = 0;
+            Entry current1 = table[index];
+            while (current1 != null)
+            {
+                if (current1.Key == null)
+                    return true;
+                current1 = current1.Next;
+            }
+            return false;
+        }
+        int bucketIndex = GetBucketIndex((K)key);
+        Entry current2 = table[bucketIndex];
+        while (current2 != null)
+        {
+            if (current2.Key != null && current2.Key.Equals(key))
+                return true;
+            current2 = current2.Next;
+        }
+        return false;
+    }
+    //6
+    public bool ContainsValue(object value)
+    {
+        for (int i = 0; i < table.Length; i++)
+        {
+            Entry current = table[i];
+            while (current != null)
+            {
+                if (value == null)
+                {
+                    if (current.Value == null) return true;
+                }
+                else if (current.Value != null && current.Value.Equals(value))
+                { return true; }
+                current = current.Next;
+
+
+
+            }
+        }
+        return false;
+    }
+    //7 возврат множества который хранит элементы пар ключ значение 
+    public HashSet<KeyValuePair<K, V>> EntrySet()
+    {
+        var set = new HashSet<KeyValuePair<K, V>>();
+        for (int i = 0; i < table.Length; i++)
+        {
+            Entry current = table[i];
+            while (current != null)
+            {
+                set.Add(new KeyValuePair<K, V>(current.Key, current.Value));
+
+                current = current.Next;
+            }
+        }
+        return set;
+    }
+    //8 возврат значения по ключу
+    public V Get(object key)
+    {
+        if (key == null)
+        {
+            int index = 0;
+            Entry current1 = table[index];
+            while (current1 != null)
+            {
+                if (current1.Key == null)
+                    return current1.Value;
+                current1 = current1.Next;
+            }
+            return default(V);
+        }
+        int bucketIndex = GetBucketIndex((K)key);
+        Entry current = table[bucketIndex];
+        while (current != null)
+        {
+            if (current != null && current.Key.Equals(key))
+                return current.Value;
+            current = current.Next;
+
+        }
+        return default(V);
+    }
+    //9
+    public bool IsEmpty()
+    {
+
+        return size == 0;
+
+    }
+    public HashSet<K> KeySet()
+    {
+        var keySet = new HashSet<K>();
+        for (int i = 0; i < table.Length; i++)
+        {
+            Entry current = table[i];
+            while (current != null)
+            {
+                keySet.Add(current.Key);
+                current = current.Next;
+
+            }
+        }
+        return keySet;
+    }
+    //11
+    public void Put(K key, V value)
+    {
+        ResizeIfNeeded();
+        //Если этот ключ уже существует меняем значение по ключу
+        int bukcetIndex = GetBucketIndex(key);
+        Entry current = table[bukcetIndex];
+        while (current != null)
+        {
+            if (key == null)
+            {
+                if (current.Key == null)
+                {
+                    current.Value = value;
+                    return;
+                }
+            }
+            else if (current.Key != null && current.Key.Equals(key))
+            {
+                current.Value = value;
+                return;
+            }
+            current = current.Next;
+        }
+
+        //Иначе выполняем вставку в бакет как голова нового списка
+        Entry newEntry = new Entry(key, value);
+        newEntry.Next = table[bukcetIndex];
+        table[bukcetIndex] = newEntry;
+        size++;
+    }
+    //12
+    public bool Remove(object key)
+    {
+        int bucketIndex;
+        if (key == null)
+        {
+            bucketIndex = 0;
+        }
+        else
+        {
+
+            bucketIndex = GetBucketIndex((K)key);
+        }
+        Entry current = table[bucketIndex];
+        Entry prev = null;
+        while (current != null)
+        {
+            if (key == null)
+            {
+                if (current.Key == null)
+                {
+                    if (prev == null)
+                    {
+                        table[bucketIndex] = current.Next;
+                    }
+                    else
+                    {
+                        prev.Next = current.Next;
+                    }
+                    size--;
+                    return true;
+                }
+            }
+            else if (current.Key != null && current.Key.Equals(key))
+            {
+
+                if (prev == null)
+                {
+                    table[bucketIndex] = current.Next;
+                }
+                else
+                {
+                    prev.Next = current.Next;
+                }
+                size--;
+                return true;
+
+            }
+            prev = current;
+            current = current.Next;
+        }
+        return false;
+    }
+    //13
+    public int Size()
+    {
+        return size;
+    }
 }
+    class Program {
+        static void Main() {
+            var map = new MyHashMap<string, int>();
+
+            map.Put("one", 1);
+            map.Put("two", 2);
+            map.Put("three", 3);
+
+            Console.WriteLine(map.Get("one"));
+            Console.WriteLine(map.Get("two"));
+            Console.WriteLine(map.Get("three"));
+
+        Console.WriteLine(map.ContainsValue(3));
+        Console.WriteLine(map.ContainsValue(4));
+        Console.WriteLine(map.ContainsKey("five"));
+        
+        map.Remove("three");
+
+        Console.WriteLine("Все ключи:");
+        var keys=map.KeySet();
+        foreach (var key in keys) {
+            Console.Write(" "+ key);
+        }
+
+        Console.WriteLine("Все пары ключ-значение:");
+
+        var entries = map.EntrySet();
+        foreach (var entry in entries)
+        {
+            Console.WriteLine($"  {entry.Key} -> {entry.Value}");
+        }
+        map.Clear();
+        Console.WriteLine(map.IsEmpty());
+    }
+    }
+    
+
+
+
