49284aacefd7c6ee1469763ae3a1dd892b1355a9
michikatsu000 <antonvsivcev2@gmail.com>
2026-04-09 17:58:14 +0300

    Task_23

--- Program.cs
+++ Program.cs
@@ -1,56 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
-
-
-namespace Task_22
+namespace Task_23
 {
     internal class Program
     {
+        public enum VarType {
+        Int,
+        Float,
+        Double,
+        Unknown
+        }
+        public class VariableInfo {
+            public VarType Type { get; set; }
+            public string Value { get; set; }
+            public VariableInfo(VarType type, string value) {
+                Type = type;
+                Value = value;
+            }
+        }
+        static void CreateExampleFile(string filename)
+        {
+            string exampleContent = @"int count = 100;
+float price = 250;
+double distance = 5000;
+int count = 200;
+string name = test;
+int score = 300;";
+
+            File.WriteAllText(filename, exampleContent);
+            Console.WriteLine($"Создан файл-пример: {filename}\n");
+        }
         static void Main(string[] args)
         {
-            string path = "C:\\Users\\Антон\\Desktop\\map.txt";
-            if (!File.Exists(path)) {
-                Console.WriteLine("Файл не найден");
-                return;
-            }
-            MyHashMap<string,int>tag= new MyHashMap<string,int>();
-            string pattern = @"</?[a-zA-Z][a-zA-Z0-9]*>";
+            string inputPath = "map2.txt";
+            string outputPath = "output.txt";
 
-            try
+            if (!File.Exists(inputPath)) {
+                Console.WriteLine($"Файл {inputPath} не найден");
+                CreateExampleFile(inputPath);
+            }
+            MyHashMap<string,VariableInfo> variabes= new MyHashMap<string,VariableInfo>();
+
+            //Мн-во допустимых типов 
+            HashSet<string> validTypes = new HashSet<string>
             {
-                string[] lines = File.ReadAllLines(path);
-                foreach (string line in lines)
+                "int", "float", "double"
+            };
+
+            string content = File.ReadAllText(inputPath);
+            // \b -граница слова 
+            // группа допустимых слов(int|float|double) |-или
+            // \b-граница слова 
+            // \s+- (\s-пробел таб или перенос строки )+-один или более
+            // потом первая буква или нижнее подчёркивание _
+            // \w*- буквы/цифры/_ *-сколько угодно 
+            // \s*-сколько угодно пробелов
+            // знак равенства =
+            // \s*-сколько угодно пробелов 
+            // группа (\d+) - \d-цифра + одна и более
+            // \s* сколько угодно пробелов
+            // ; - конец выражения в языке проги
+            string pattern = @"\b(int|float|double)\b\s+([a-zA-Z_]\w*)\s*=\s*(\d+)\s*;";
+            var matches=Regex.Matches(content, pattern);
+            foreach (Match match in matches) {
+            string type = match.Groups[1].Value;
+            string name=match.Groups[2].Value;
+            string value = match.Groups[3].Value;
+                if (!validTypes.Contains(type))
                 {
-                    MatchCollection matches = Regex.Matches(line, pattern);
-                    foreach (Match match in matches)
-                    {
-                        string tagg = match.Value;
-                        string normalizteg = tagg.Replace("/", "").Replace("<", "").Replace(">", "").ToLower();
-                        int currentCount = tag.Get(normalizteg);
-                        if (currentCount == default(int))
-                        {
-                            tag.Put(normalizteg, 1);
-                        }
-                        else
-                        {
-                            tag.Put(normalizteg, currentCount + 1);
-                        }
-                    }
+                    Console.WriteLine($"Недопустимый тип");
+                    continue;
+                }
+              VarType type2=VarType.Unknown;
+                if (type == "int") type2 = VarType.Int;
+                else if (type=="float") type2= VarType.Float;
+                else if (type=="double") type2= VarType.Double;
+                if (variabes.ContainsKey(name)) {
+                    Console.WriteLine($"Переопределение переменной: {name}");
+                    continue;
+                }
+                variabes.Put(name,new VariableInfo(type2,value));
+            }
+            using (StreamWriter wr = new StreamWriter(outputPath))
+            { 
+            var entries=variabes.EntrySet();
+                foreach (var entry in entries){
+                    string typestr = entry.Value.Type.ToString().ToLower();
+                    wr.WriteLine($"{typestr}=> {entry.Key}({entry.Value.Value})");
                 }
             }
-            catch (Exception ex) {
-                Console.WriteLine("Ошибка чтения файла"+ex.Message);
-            }
-            var set =tag.EntrySet();
-            foreach (var t in set) {
-                Console.WriteLine($"{t.Key}->{t.Value}");
-            }
+            Console.WriteLine("\nРезультат записан в файл output.txt");
         }
     }
 }
